693208f10a84293238f4db00e23c1ea553038e12
Sarraalii <sarra.ali@mail.utoronto.ca>
2023-12-11 14:52:10 -0500

    Updating variables

--- R/data.R
+++ R/data.R
@@ -3,8 +3,9 @@
 #'   Data for 181 countries from the Uppsala Conflict Data Program and World Bank for the years 2000 to 2019.
 #'   The dataset includes various socio-economic indicators.
 
-#' @format:
+#' @format
 #'   A data frame with 3721 rows and 20 columns
+#'   Variables:country_name: Country Name, ISO: ISO Country Code, region: Region, Year: Year, GDP: Gross Domestic Product, OECD: Organization for Economic Co-operation and Development Membership, OECD2023: OECD Membership Status in 2023, popdens: Population Density, urban: Urban Population, agedep: Age Dependency Ratio, male_edu: Male Education, temp: Temperature, totdeath: Total Deaths, armconf1: Armed Conflict Level 1, drought: Drought Occurrence, earthquake: Earthquake Occurrence, MatMort: Maternal Mortality, InfantMort: Infant Mortality, NeoMort: Neonatal Mortality, Under5Mort: Under-5 Mortality`
 #' @source: The data is sourced from the Uppsala Conflict Data Program and the World Bank (https://pubmed.ncbi.nlm.nih.gov/34582455/).
 
 
